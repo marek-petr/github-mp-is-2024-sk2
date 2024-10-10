cbad60831b73ea00c3c114599aea3d8fdc241056
Marek Petr <marek.petr@tul.cz>
2024-10-10 13:32:23 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+class Program {
+
+    static void Main() {
+        // chci aby se program opakoval po stisku klavesi "a"
+        string again = "a";
+        while (again == "a") {
+            //Console.Clear();
+            Console.WriteLine("##################");
+            Console.WriteLine("####Výpis řady####");
+            Console.WriteLine("##################");   
+            Console.WriteLine("####Marek Petr####");
+            Console.WriteLine("##################\n\n");   
+            Console.WriteLine();
+
+        // Vstup od uživatele - TO-DO            
+
+        // logika pro výpis řady - TO-DO
+
+
+        // Opakování programu
+        Console.WriteLine("Pro opakovani programu stisknete klavesu a");
+        again = Console.ReadLine();
+        }
+    }
+}
